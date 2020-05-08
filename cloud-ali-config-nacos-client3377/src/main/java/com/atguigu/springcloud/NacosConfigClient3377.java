13ffbf2a054f8e036f47975a462a6d5d94c53838
Guimin Chu <455824173@qq.com>
2020-05-08 12:38:41 +0800

    Day 9, Nacos 配置中心

---
+++ cloud-ali-config-nacos-client3377/src/main/java/com/atguigu/springcloud/NacosConfigClient3377.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description: 启动类
+ * @Author: cgm
+ * @Date: 2020/3/14 11:09
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClient3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClient3377.class, args);
+    }
+}
---
+++ cloud-ali-config-nacos-client3377/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,30 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: cgm
+ * @Date: 2020/5/8 11:00
+ */
+@Slf4j
+@RefreshScope
+@RestController
+@RequestMapping("/nacos")
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+
+}
+
